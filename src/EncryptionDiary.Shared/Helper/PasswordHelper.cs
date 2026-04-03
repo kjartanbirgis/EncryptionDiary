9a528a6b445212c054bb21338c5ba3a743a6b9ac
Kjartan Birgisson <kjartan@stubbur.com>
2026-04-03 13:38:05 +0000

    Bætti við Password helper til að hash lykilorð og generata salt.

---
+++ src/EncryptionDiary.Shared/Helper/PasswordHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace EncryptionDiary.Shared.Helper
+{
+    public static class PasswordHelper
+    {
+        public static byte[] GenerateSalt(int size = 32)
+        {
+            return RandomNumberGenerator.GetBytes(size);
+        }
+
+        public static byte[] HashPassword(byte[] clientHash, byte[] salt,int iterations,string? pepper = null)
+        {
+            byte[] input;
+            if (pepper != null)
+            {
+                var pepperBytes = Encoding.UTF8.GetBytes(pepper);
+                input =new byte[clientHash.Length + pepperBytes.Length];
+                clientHash.CopyTo(input, 0);
+                pepperBytes.CopyTo(input, clientHash.Length);
+            }
+            else
+            {
+                input = clientHash;
+            }
+
+            using var pbkdf2 = new Rfc2898DeriveBytes(input,salt,iterations,HashAlgorithmName.SHA256);
+            return pbkdf2.GetBytes(32); //32 is for SHA256 (8*32 = 256)
+        }
+    }
+}
