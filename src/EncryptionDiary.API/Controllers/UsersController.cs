924f9492a9f504c6ed7412f489a3ec3132ed758f
Kjartan Birgisson <kjartan@stubbur.com>
2026-04-03 13:45:41 +0000

    Hopefully RegisterUser is finished

--- src/EncryptionDiary.API/Controllers/UsersController.cs
+++ src/EncryptionDiary.API/Controllers/UsersController.cs
@@ -1,5 +1,8 @@
 ﻿using EncryptionDiary.API.Repository;
+using EncryptionDiary.Shared.Helper;
+using EncryptionDiary.Shared.Models;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace EncryptionDiary.API.Controllers
 {
@@ -8,11 +11,14 @@ namespace EncryptionDiary.API.Controllers
     public class UsersController : Controller
     {
         private readonly UserRepository _userRepository;
+        private readonly IConfiguration _configuration;
 
-        public UsersController (UserRepository userRepository)
+        public UsersController (UserRepository userRepository, IConfiguration configuration)
         {
             _userRepository = userRepository;
+            _configuration = configuration;
         }
+        
         [HttpGet("{Username}")]
         public async Task<IActionResult> GetUser(string username)
         {
@@ -22,6 +28,26 @@ namespace EncryptionDiary.API.Controllers
                 return NotFound();
             }
             return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterUser(RegisterUser user)
+        {
+            if (user == null) { return BadRequest(); }
+
+            var iteration = _configuration.GetValue<int>("PasswordSettings:Iterations");
+            var pepper = _configuration.GetValue<string>("PasswordSettings:Pepper");
+            var salt = PasswordHelper.GenerateSalt();
+            var hash = PasswordHelper.HashPassword(user.ClientHash, salt, iteration, pepper);
+
+            var registerUser = new User()
+            {
+                Username = user.Username,
+                PasswordHash= hash,
+                PasswordSalt= salt,
+                PasswordIteration= iteration
+            };
+            return Ok(await _userRepository.RegisterUser(registerUser));
         }
     }
 }
