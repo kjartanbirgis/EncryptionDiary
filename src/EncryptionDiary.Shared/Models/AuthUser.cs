537b68c406ecb12bf4cd8c8f1da4de331955aecb
Kjartan Birgisson <kjartan@stubbur.com>
2026-04-03 13:51:25 +0000

    Ákvað að endurskýra skránna og clasann

---
+++ src/EncryptionDiary.Shared/Models/AuthUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionDiary.Shared.Models
+{
+    public class AuthUser
+    {
+        public string Username { get; set; }
+        public byte[] ClientHash { get; set; }
+    }
+}
