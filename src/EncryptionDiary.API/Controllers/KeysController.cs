a8941acc7ac8ed7e6d5ef676fe95017ee83073da
Kjartan Birgisson <kjartan@stubbur.com>
2026-04-04 16:22:08 +0000

    ekki byrjaður

---
+++ src/EncryptionDiary.API/Controllers/KeysController.cs
@@ -0,0 +1,18 @@
+﻿using EncryptionDiary.API.Repository;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace EncryptionDiary.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class KeysController : Controller
+    {
+        private readonly KeyRepository _keyRepository;
+
+        public KeysController(KeyRepository keyRepository)
+        {
+            _keyRepository = keyRepository;
+        }
+    }
+}
