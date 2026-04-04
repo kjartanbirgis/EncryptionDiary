6da2b70cdda808de7e42dae4ee99bc02d52d1dd2
Kjartan Birgisson <kjartan@stubbur.com>
2026-04-04 16:21:57 +0000

    Bæta við

---
+++ src/EncryptionDiary.API/Repository/DiaryAssetsRepository.cs
@@ -0,0 +1,29 @@
+﻿using EncryptionDiary.Shared.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace EncryptionDiary.API.Repository
+{
+    public class DiaryAssetsRepository : BaseRepository
+    {
+        public DiaryAssetsRepository(string connectionString) : base(connectionString)
+        {
+        }
+
+        public async Task<List<DiaryAssets>> GetAllDiaryAssets(Guid DiaryID)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task SoftDeleteDiaryAssets (Guid diaryAssetsID)
+        {  
+            throw new NotImplementedException();
+        }
+        public async Task<DiaryAssets> InsertDiaryAssets(DiaryAssets diaryAssets)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<DiaryAssets> ModifyDiaryAssets(DiaryAssets diaryAssets)
+        {  
+            throw new NotImplementedException(); 
+        }
+    }
+}
---
+++ src/EncryptionDiary.API/Repository/DiaryRepository.cs
@@ -0,0 +1,28 @@
+﻿using EncryptionDiary.Shared.Models;
+
+namespace EncryptionDiary.API.Repository
+{
+    public class DiaryRepository : BaseRepository
+    {
+        public DiaryRepository(string connectionString) : base(connectionString)
+        {
+        }
+
+        public async Task<List<Diary>> GetAllDiary(Guid UserID)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task SoftDeleteDiary(Guid diaryID)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<Diary> InsertDiary(Diary diary)
+        {
+            throw new NotImplementedException();
+        }
+        public async Task<Diary> ModifyDiary(Diary diary)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
