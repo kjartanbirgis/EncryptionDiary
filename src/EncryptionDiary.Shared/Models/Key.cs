bce1d8a7192f604e02689bc5ba240baa889370b1
Kjartan Birgisson <kjartan@stubbur.com>
2026-04-04 15:26:00 +0000

    Bæta við Nullable values

--- src/EncryptionDiary.Shared/Models/Key.cs
+++ src/EncryptionDiary.Shared/Models/Key.cs
@@ -8,15 +8,15 @@ namespace EncryptionDiary.Shared.Models
 {
     public class Key
     {
-        public Guid ID { get; set; }
+        public Guid? ID { get; set; }
         public Guid UserID { get; set; }
         public byte[]? EncKey { get; set; }
         public byte[]? KeyNonce { get; set; }
         public byte[]? KeyTag { get; set; }
         public string? Description { get; set; } //hum þetta ætti kanski ekki að vera í gagnagrunninum heldur í clientinum???
         public bool? Shared { get; set; } //sama hér??
-        public DateTime Created { get; set; }
-        public DateTime Updated { get; set; }
+        public DateTime? Created { get; set; }
+        public DateTime? Updated { get; set; }
         public DateTime? Deleted { get; set; }
     }
 }
