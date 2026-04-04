96f8b2c18fa853ef517c34914e09da76730c09cd
Kjartan Birgisson <kjartan@stubbur.com>
2026-04-04 14:10:11 +0000

    Gleymdi að Keys mætti vera null

--- src/EncryptionDiary.Shared/Models/Key.cs
+++ src/EncryptionDiary.Shared/Models/Key.cs
@@ -10,9 +10,9 @@ namespace EncryptionDiary.Shared.Models
     {
         public Guid ID { get; set; }
         public Guid UserID { get; set; }
-        public byte[] EncKey { get; set; }
-        public byte[] KeyNonce { get; set; }
-        public byte[] KeyTag { get; set; }
+        public byte[]? EncKey { get; set; }
+        public byte[]? KeyNonce { get; set; }
+        public byte[]? KeyTag { get; set; }
         public string? Description { get; set; } //hum þetta ætti kanski ekki að vera í gagnagrunninum heldur í clientinum???
         public bool? Sheared { get; set; } //sama hér??
         public DateTime Created { get; set; }
