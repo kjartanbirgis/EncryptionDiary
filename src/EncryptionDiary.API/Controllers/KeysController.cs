27acb66c19e4eab5c9f1e228e19ab5eaa89160c7
Kjartan Birgisson <kjartan@stubbur.com>
2026-04-05 16:16:18 +0000

    Var að gera eitthvað man ekki hvað

--- src/EncryptionDiary.API/Controllers/KeysController.cs
+++ src/EncryptionDiary.API/Controllers/KeysController.cs
@@ -1,6 +1,10 @@
 ﻿using EncryptionDiary.API.Repository;
+using EncryptionDiary.Shared.Helper;
+using EncryptionDiary.Shared.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Npgsql.Replication.PgOutput.Messages;
+using System.Security.Cryptography;
 
 namespace EncryptionDiary.API.Controllers
 {
@@ -14,5 +18,37 @@ namespace EncryptionDiary.API.Controllers
         {
             _keyRepository = keyRepository;
         }
+
+        [HttpGet("all")]
+        public async Task<IActionResult> RetreveKeys(Guid userID)
+        {
+            return Ok(await _keyRepository.GetAllKeys(userID));
+        }
+
+        [HttpGet("{keyID}")]
+        public async Task<IActionResult> GetKeys(Guid keyID)
+        {
+            return Ok(await _keyRepository.GetKeyByID(keyID));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> InsertKey([FromBody] Key keyData)
+        {
+            return Ok(await _keyRepository.InsertKey(keyData));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> ModifyKey([FromBody] Key keyData)
+        {
+            return Ok(await _keyRepository.ModifyKeyByID(keyData));
+        }
+        [HttpDelete("{keyID}")]
+        public async Task<IActionResult> DeleteKey(Guid keyID)
+        {
+            var returnValue = await _keyRepository.SoftDeleteKeyByID(keyID);
+            return returnValue ? Ok() : NotFound();
+
+        }
+
     }
 }
