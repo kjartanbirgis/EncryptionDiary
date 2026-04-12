c4d7da7a0afdb59d2577cf2d5e510ac04049cbd2
Kjartan Birgisson <kjartan@stubbur.com>
2026-04-12 13:30:46 +0000

    Add small check if user is if he says who he is just 1 check should be on more.

--- src/EncryptionDiary.API/Controllers/DiariesController.cs
+++ src/EncryptionDiary.API/Controllers/DiariesController.cs
@@ -2,6 +2,7 @@
 using EncryptionDiary.Shared.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace EncryptionDiary.API.Controllers
 {
@@ -18,10 +19,15 @@ namespace EncryptionDiary.API.Controllers
         {
             _diaryRepository = diaryRepository;
         }
-
+        //svona loka á að það se ekki hægt að gera þetta skipta um userID
         [HttpGet("all")]
         public async Task<IActionResult> RetreveDaries(Guid userID)
         {
+            var tokenUserId = GetUserIdFromToken();
+            if (tokenUserId != userID)
+            { 
+            return Forbid(); // 403 - tampering detected //spurning um að logga þetta svo hægt væri að loka á iptölur sem eru í svona vinnu.
+        }
             return Ok(await _diaryRepository.GetAllDiaries(userID));
         }
 
@@ -48,6 +54,11 @@ namespace EncryptionDiary.API.Controllers
             var returnValue = await _diaryRepository.SoftDeleteDiaryByID(diaryID);
             return returnValue ? Ok() : NotFound();
 
+        }
+        private Guid GetUserIdFromToken()
+        {
+            var claim = User.FindFirst(ClaimTypes.NameIdentifier);
+            return Guid.Parse(claim.Value);
         }
     }
 }
